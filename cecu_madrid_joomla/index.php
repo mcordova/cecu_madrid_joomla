6833c409b1c5b0dcfab8e22924dba50f5de112db
Miguel Cordova <miguel@saftel.net>
2010-02-25 09:54:51 +0100

    se añade cabecera meta para el charset utf-8

--- cecu_madrid_joomla/index.php
+++ cecu_madrid_joomla/index.php
@@ -16,7 +16,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
    <jdoc:include type="head" />
-
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/cecu_madrid_joomla/css/template.css" type="text/css" />
